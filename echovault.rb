7af3275e27cbfb6dc88e07c3f3d239c2f6f47ce4
kelvinmwinuka <kelvin@echovault.io>
2024-07-03 01:22:45 +0800

    Brew formula update for EchoVault version v0.8.0

--- echovault.rb
+++ echovault.rb
@@ -5,11 +5,11 @@
 class Echovault < Formula
   desc ""
   homepage "https://github.com/EchoVault/EchoVault"
-  version "0.7.0"
+  version "0.8.0"
 
   on_macos do
-    url "https://github.com/EchoVault/EchoVault/releases/download/v0.7.0/EchoVault_Darwin_all.tar.gz"
-    sha256 "147aa65ff0320d48de21647d2579a116aabce1d2e6ea53f77cb51ac0c124cc3b"
+    url "https://github.com/EchoVault/EchoVault/releases/download/v0.8.0/EchoVault_Darwin_all.tar.gz"
+    sha256 "126451e1583682f484c12c19b39c4e25637ca3e414ae7142eac5ca39b20ed776"
 
     def install
       bin.install "EchoVault"
@@ -19,8 +19,8 @@ class Echovault < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/EchoVault/EchoVault/releases/download/v0.7.0/EchoVault_Linux_x86_64.tar.gz"
-        sha256 "0b90114d084ed0363f286f6bde5b475c25befc5f1b77e80d1008ea3c8c81cda7"
+        url "https://github.com/EchoVault/EchoVault/releases/download/v0.8.0/EchoVault_Linux_x86_64.tar.gz"
+        sha256 "82df63219c2fa8397799cd1dbafe23912112299bcddcc5fe1af2b6a2ea72bd0b"
 
         def install
           bin.install "EchoVault"
@@ -29,8 +29,8 @@ class Echovault < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/EchoVault/EchoVault/releases/download/v0.7.0/EchoVault_Linux_arm64.tar.gz"
-        sha256 "ebee9871839796da9e8c0364b405fa2af81aba32fd876ff9aa38d08736c63ffe"
+        url "https://github.com/EchoVault/EchoVault/releases/download/v0.8.0/EchoVault_Linux_arm64.tar.gz"
+        sha256 "b4be6adede28f659c7d57bcb243e9a51b1700dcfb06a9bf86a6e5b672cee4562"
 
         def install
           bin.install "EchoVault"
